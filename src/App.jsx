5c2e0481e8fc8e0f643c1dacd0d1f4312962a65f
Nicolas Martins <nicolasdemartins@hotmail.com>
2024-03-24 21:24:59 -0300

    projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import Suvs from "./components/Suvs"
 export default function  App() {
   return (
     <main className="min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex justify-center itens-center">
-      <section className="lg:flex">
+      <div className="lg:flex">
         <Sedans/>
         <Suvs/>
         <Luxuosos/>
-      </section>
+      </div>
     </main>
   )
 }
